972f4d394144868fca884efc0fec87d91c433c93
Hindkaer <hindkaer_msn@hotmail.com>
2018-08-30 09:37:58 +0200

    My first GUI commit

---
+++ Desktop/IKT/4Semester/I4GUI/Week1/ConsoleAppAdder/ConsoleAppAdder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleAppAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st number:");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Enter 2nd number:");
+            string b = Console.ReadLine();
+
+            int valueA = int.Parse(a);
+            int valueB = int.Parse(b);
+
+            int c = valueA + valueB;
+
+            Console.WriteLine("The sum of 1 and 2 is {0}.", c);
+        }
+    }
+}
